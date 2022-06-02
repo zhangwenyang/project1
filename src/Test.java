a73f41ec15d85a8aad6b59f837ec1ecea1a668e2
zwy <335404259@qq.com>
2022-06-02 12:38:47 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        System.out.println("Test.main");
+
+    }
+
+    //你好啊
+    public void f1(){
+    }
+
+    /*反对法地方*/
+    public void f2(){
+
+    }
+}
---
+++ src/Test2.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author zwy
+ * @create 2022/6/2
+ *
+ * sout,soutv,soutm
+ *
+ *
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+        System.out.println("args = " + Arrays.deepToString(args));
+
+        System.out.println("args = " + args);
+
+        System.out.println("args = " + args);
+
+        int num = 0;
+        int num2 = 3;
+        System.out.println("num2 = " + num2);
+        
+        System.out.println(num);
+        System.out.println("num = " + num);
+        System.out.println("num = " + num);
+
+        for (String arg : args) {
+
+        }
+
+        ArrayList<Integer> strings = new ArrayList<>();
+        strings.add(1);
+        strings.add(2);
+        strings.add(3);
+        strings.add(4);
+        strings.add(5);
+
+        for (Integer string : strings) {
+            System.out.println(string);
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            
+        }
+
+        System.out.println("strings = " + strings);
+
+        for (Integer string : strings) {
+            
+        }
+
+        for (Integer string : strings) {
+            
+        }
+
+        if (strings == null) {
+            
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+        }
+
+        if (strings != null) {
+
+        }
+        if (strings != null) {
+
+        }
+
+        if (strings != null) {
+
+        }
+        if (strings == null) {
+
+        }
+
+    }
+
+}
